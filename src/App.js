88a5ddb8ce285182a415f3eee06be5bf26fc8e62
Corinne Krych <corinnekrych@gmail.com>
2020-09-23 08:21:42 +0200

    initial repo with  0.0.13-alpha-3 for react-honeyui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import add from "@cloudbees/honeyui-icons/svg/add.svg";
+import Icon from "@cloudbees/react-honeyui";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
+        <Icon currentColor width={30} height={30} viewBox={add.viewBox} id={add.id} />
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
